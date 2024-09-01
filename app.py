10d0e35e53f2df751bfea144477608b48ca9bd2a
wellrestedmango <ericfassbender@proton.me>
2024-08-31 21:38:43 -0500

    starting on css redesign

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 from unicodedata import category
-
 from docutils.nodes import description
 from flask import Flask, render_template, url_for, redirect, request
 from flask_sqlalchemy import SQLAlchemy
-import os
-import datetime
-from datetime import datetime
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
+from sqlalchemy import update
+from jeepney.bindgen import Method
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI']=\
-#    'sqlite:///' + os.path.join(basedir, 'database.db')
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+
 
 #to create or change do the following
 #in terminal export FLASK_APP=app
@@ -23,6 +17,7 @@ db = SQLAlchemy(app)
 #from app import db, Task
 #db.create_all() to create
 #db.drop_all() if you need to drop and restart
+
 
 
 class Task(db.Model):
@@ -40,11 +35,6 @@ class Task(db.Model):
             self.complete,
             self.category
         )
-
-
-
-
-
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -74,6 +64,8 @@ def landing():
         return render_template('index.html', task_list=task_list)
 
 
+
+
 @app.route('/delete/<int:id>')
 def delete(id):
     task_to_delete = Task.query.get_or_404(id)
@@ -84,6 +76,30 @@ def delete(id):
         return redirect('/')
     except:
         return 'failed to delete'
+
+
+@app.route('/update/<int:id>', methods=['POST', 'GET'])
+def update(id):
+    task_to_update = Task.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task_to_update.title = request.form['title']
+        task_to_update.description = request.form['description']
+        task_to_update.due = request.form['due-date']
+        task_to_update.category = request.form['task-category']
+
+        try:
+            #db.session.update(task_to_update)
+            db.session.commit()
+            return redirect('/')
+
+        except:
+            return 'There was an issue adding this task. Please reload'
+
+    else:
+        return render_template('update.html', task=task_to_update)
+
+
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True)
